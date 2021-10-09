0930c3bf455fa715477bc11e76fc546f911a2610
Haixin Shi <sysonsoar@gmail.com>
2021-10-09 12:02:21 +0200

    connected but not synchronized!

--- template_cpp/src/include/flp2p.hpp
+++ template_cpp/src/include/flp2p.hpp
@@ -27,8 +27,8 @@ private:
 	string output;//log file address
 	string config;//determine the behaviours of process
 	int send_seq=1;
-	int recv_seq=1;
 	ofstream log;
+	int s;
 public:
 	//creator funtion of flp2p class
 	flp2p(unsigned long myID, vector<myhost>* hosts, const char* output, const char* config){
@@ -38,43 +38,92 @@ public:
 		this->output = output;
 		this->config = config;
 		this->log.open(this->output);
-		cout << "send thread init start" << endl;
+
+		//bind the listening socket
+		this->s = socket(AF_INET, SOCK_DGRAM, 0);
+		for(unsigned int i=0; i<this->hosts.size(); i++){
+			if(this->hosts[i].id == this->myID) {
+				struct sockaddr_in addr;
+			    socklen_t addr_len = sizeof(addr);
+			    memset(&addr, 0, sizeof(addr));
+			    addr.sin_family = AF_INET; // Use IPV4
+			    addr.sin_port = this->hosts[i].port;
+			    //inet_aton(ip, &myaddr.sin_addr.s_addr);
+			    addr.sin_addr.s_addr = this->hosts[i].ip;
+				if( bind(this->s , reinterpret_cast<struct sockaddr *>(&addr), addr_len ) == -1)
+				{
+					cout<<"bind error!" << endl;
+				}				
+			}
+				
+		}		
+
+
+		cout << "sending thread init start" << endl;
 		thread sendthread(flp2pSend, this);
-		cout << "send thread init finish" << endl;
+		cout << "sending thread init finish" << endl;
 
 		cout << "receive thread init start" << endl;
 		vector<thread> recvthreads;
 		for(unsigned int i=0; i<this->hosts.size(); i++){
-			if(this->hosts[i].id != this->myID)
+			if(this->hosts[i].id != this->myID) 
 				recvthreads.push_back(thread(flp2pDeliver, this, this->hosts[i]));
 		}
 		cout << "receive thread init finish" << endl;		
+		
+		//join the threads
 		sendthread.join();
-		for(unsigned int i=0; i<this->hosts.size(); i++){
+		for(unsigned int i=0; i<recvthreads.size(); i++){
 			recvthreads[i].join();
 		}
+		//testing
+		/*
+		this->s = socket(AF_INET, SOCK_DGRAM, 0);
+		if(myID == 1){
+			cout << "enter Process 1" << endl;
+			thread th1(flp2pSend, this);
+			th1.join();
+		}
+		if(myID == 2){
+			cout << "enter Process 2" << endl;
+			struct sockaddr_in addr;
+		    socklen_t addr_len = sizeof(addr);
+		    memset(&addr, 0, sizeof(addr));
+		    addr.sin_family = AF_INET; // Use IPV4
+		    addr.sin_port = this->hosts[1].port;
+		    //inet_aton(ip, &myaddr.sin_addr.s_addr);
+		    addr.sin_addr.s_addr = this->hosts[1].ip;
+			if( bind(this->s , reinterpret_cast<struct sockaddr *>(&addr), addr_len ) == -1)
+			{
+				cout<<"bind error!" << endl;
+			}
+			thread th2(flp2pDeliver, this, this->hosts[0]);
+			th2.join();
+		}*/
 		
 	}
 	~flp2p(){
 		log.close();
 	}
 	void UDPSend(int s, in_addr_t ip, unsigned short port, char* message){
-
 		if(s == -1){
 			cout<<"could not create socket while sending";
 			return;
 		}
-
 		//specify address
 		struct sockaddr_in addr;
-	    socklen_t addr_len=sizeof(addr);
+	    socklen_t addr_len = sizeof(addr);
 	    memset(&addr, 0, sizeof(addr));
 	    addr.sin_family = AF_INET; // Use IPV4
-	    addr.sin_port = htons(port);
+	    addr.sin_port = port;
 	    addr.sin_addr.s_addr = ip;
 
 	    //send the message to the address
-	    sendto(s, message, sizeof(message), 0, reinterpret_cast<struct sockaddr *>(&addr), addr_len);
+	    ssize_t ret = 0;
+	    ret = sendto(s, message, sizeof(message), 0, reinterpret_cast<struct sockaddr *>(&addr), addr_len);
+		if(ret == -1){
+			cout << "sending encounter errors!" << endl;
+		}
 	}
 	void UDPReceive(int s, in_addr_t ip, unsigned short port, char* buffer){
 		cout << "enter function : UDPReceive" << endl;
@@ -88,13 +137,11 @@ public:
 	    socklen_t addr_len = sizeof(addr);
 	    memset(&addr, 0, sizeof(addr));
 	    addr.sin_family = AF_INET; // Use IPV4
-	    addr.sin_port = htons(port);
+	    addr.sin_port = port;
 	    //inet_aton(ip, &myaddr.sin_addr.s_addr);
 	    addr.sin_addr.s_addr = ip;
-	    cout << "1" << endl;
 	    //receive the message from the address
 	    recvfrom(s, buffer, sizeof(buffer), 0, reinterpret_cast<struct sockaddr *>(&addr), &addr_len);
-		cout << "2" << endl;
 	}
 	static void flp2pSend(flp2p* thiz){
 		//create socket
@@ -102,8 +149,14 @@ public:
 		while(1)//single thread or multi threads
 		for (auto &host : thiz->hosts){
 			if(host.id != thiz->myID){
-				//message only include current Process ID 			
-				thiz->UDPSend(s, host.ip, host.port, reinterpret_cast<char*>(&thiz->myID));
+				//message only include current Process ID
+				string seq = to_string(thiz->send_seq);
+				char* sendmsg = new char[seq.size()+1];
+				for(unsigned int i = 0; i<seq.size(); i++){
+					sendmsg[i] = seq[i];
+				}
+				sendmsg[seq.size()]='\0';		
+				thiz->UDPSend(s, host.ip, host.port,sendmsg);
 				//log this send event
 				string tag = "b ";
 				string loginfo = tag + to_string(thiz->send_seq);
@@ -113,19 +166,14 @@ public:
 		}
 	}
 	static void flp2pDeliver(flp2p* thiz, myhost host){
-		//create socket
-		int s = socket(AF_INET, SOCK_DGRAM, 0);
 		while(1){//single thread or multi threads
-			char recvinfo[256];
+			char recvinfo[10];
 			//message only include send Process ID 			
-			thiz->UDPReceive(s, host.ip, host.port,recvinfo);
+			thiz->UDPReceive(thiz->s, host.ip, host.port,recvinfo);
 			//log this receive event
 			string tag = "d ";
-			cout << "Deliver is writing!" << endl;
-			string loginfo = tag + recvinfo + to_string(thiz->recv_seq);
+			string loginfo = tag + to_string(host.id) +" "+ recvinfo;
 			thiz->log << loginfo << endl;
-			cout << loginfo << endl;
-			thiz->recv_seq++;
 		}
 	}
 };
